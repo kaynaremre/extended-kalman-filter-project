b4a59ba3a80fd30d3f5ea7aa3bf60dab7bad845e
root <root@EMRE-LAPTOP.localdomain>
2021-05-28 03:37:19 +0300

    jacobian fix

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -170,7 +170,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // TODO: Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
-    
     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
