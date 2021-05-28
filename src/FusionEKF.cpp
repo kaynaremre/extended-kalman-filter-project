a328176b2bbd255d321cfb369b246f0649a1ecd5
root <root@EMRE-LAPTOP.localdomain>
2021-05-28 03:36:35 +0300

    debug

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,7 +36,7 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    */
 
-  ekf_.x_ = VectorXd(4);
+  //ekf_.x_ = VectorXd(4);
 
   H_laser_ << 1, 0, 0, 0,
               0, 1, 0 ,0;
@@ -105,7 +105,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
               0, 1, 0, 1,
               0, 0, 1, 0,
               0, 0, 0, 1;
-  
+
     previous_timestamp_ = measurement_pack.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -131,8 +131,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
 
-  float noise_ax = 9;
-  float noise_ay = 9;
+  float noise_ax = 15;
+  float noise_ay = 15;
 
   ekf_.Q_ = MatrixXd::Zero(4,4);
 
@@ -159,7 +159,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
-    ekf_.H_ = tools.CalculateJacobian(Hj_);
+
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+
     ekf_.R_ = R_radar_;
 
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -47,7 +47,7 @@ void KalmanFilter::Update(const VectorXd &z) {
 
   //new estimate
   x_ = x_ + (K * y);
-  long x_size = x_.size();
+  int x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
 
@@ -78,7 +78,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   MatrixXd K = PHt * Si;
 
   x_ = x_ + (K * y);
-  long x_size = x_.size();
+  int x_size = x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * H_) * P_;
 
